62948b54e69d7e4dbd0b4759084c49cf0b422661
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-21 13:01:26 +0100

    Music

---
+++ RESTPP/Models/Music.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace RESTPP.Models
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        public string Artist { get; set; }
+        public int PublishingYear { get; set; }
+
+        public Music()
+        {
+
+        }
+        
+        public Music(int id, string title, string artist, int publishingYear)
+        {
+            Id = id;
+            Title = title;
+            Artist = artist;
+            PublishingYear = publishingYear;
+        }
+    }
+}
