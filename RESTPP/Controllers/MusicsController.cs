6b44dc0757ea531c026655810c94aeccb393ee4e
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-21 13:13:23 +0100

    getall

--- RESTPP/Controllers/MusicsController.cs
+++ RESTPP/Controllers/MusicsController.cs
@@ -10,35 +10,35 @@ namespace RESTPP.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class MusicsController : ControllerBase
     {
-        // GET: api/<ValuesController>
+        // GET: api/<MusicsController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/<ValuesController>/5
+        // GET api/<MusicsController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST api/<ValuesController>
+        // POST api/<MusicsController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/<MusicsController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/<MusicsController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- RESTPP/Managers/MusicsManager.cs
+++ RESTPP/Managers/MusicsManager.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RESTPP.Models;
 
 namespace RESTPP.Managers
 {
     public class MusicsManager
     {
+        public static int nextId=1;
+
+        private static readonly List<Music> data = new List<Music>
+        {
+            new Music {Id = nextId++, Title = "first", Artist = "la", PublishingYear = 1369},
+            new Music {Id = nextId++, Title = "first", Artist = "la", PublishingYear = 1369},
+
+        };
+
+        public IEnumerable<Music> GetAll()
+        {
+
+        }
     }
 }
