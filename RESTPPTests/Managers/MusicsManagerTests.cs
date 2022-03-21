bf8b341e6332e96c33ff8c3df15994ee13a94381
Lava Maamo <lama26830@edu.ucl.dk>
2022-03-21 14:34:31 +0100

    getalltest

--- RESTPPTests/Managers/MusicsManagerTests.cs
+++ RESTPPTests/Managers/MusicsManagerTests.cs
@@ -1,21 +1,34 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RESTPP.Managers;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RESTPP.Models;
 
 namespace RESTPP.Managers.Tests
 {
     [TestClass()]
     public class MusicsManagerTests
     {
+        private MusicsManager manager;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            manager = new MusicsManager();
+        }
 
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            IEnumerable<Music> musics = manager.GetAll("first");
+            foreach (Music e in musics)
+            {
+                Assert.IsTrue(e.Title == "first");
+            }
         }
 
         [TestMethod()]
